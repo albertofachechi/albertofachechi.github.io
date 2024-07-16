a4a078437c5b0dffdc44a35238c56b4d9e516676
albertofachechi <alberto.fachechi@gmail.com>
2024-07-17 01:10:35 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $secretKey = "";
+    $secretKey = "6LfWoREqAAAAAH6DUDG-izDORk-xN3VrR7b0bX7T";
     $responseKey = $_POST['g-recaptcha-response'];
     $userIP = $_SERVER['REMOTE_ADDR'];
 
